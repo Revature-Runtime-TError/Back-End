786487bf01b5a41e75a5b2f3ddd1c2b3d3658a04
Wesley Pruett <110426206+wespruett@users.noreply.github.com>
2022-10-18 09:48:24 -0600

    added andy's back end unit tests

--- social-media/src/test/java/com/revature/service/UpdateUser.java
+++ social-media/src/test/java/com/revature/service/UpdateUser.java
@@ -1,72 +1,106 @@
 package com.revature.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import com.revature.dao.UserDao;
 import com.revature.entity.UserEntity;
 import com.revature.services.UserService;
 
+
+
+@ExtendWith(MockitoExtension.class)
 public class UpdateUser {
 
-	@Autowired 
-	UserService userService;
-	@Autowired
-	UserDao userDao;
-	
-	@Test
-	void contextLoads() {
-	}
-	@Test
-	void testUpdateLast() {
-		UserEntity user =  new UserEntity(100, "a","b","c","d", "d");
-           
-	    assertThat(user).isNotNull();
-	    
-	    user.setLastName("abc");
-	    userDao.save(user);
-	    
-	    Optional<UserEntity> optionalEntity =  userDao.findByLastName("abc");
-	    UserEntity checkLastname= optionalEntity.get();
-	    assertThat(checkLastname).isNotNull();
-	    
- 
-	}
-	
-	@Test
-	void testUpdateFirst() {
-		UserEntity user =  new UserEntity(100, "a","b","c","d", "d");
-           
-	    assertThat(user).isNotNull();
-	    
-	    user.setFirstName("abc");
-	    userDao.save(user);
-	    
-	    Optional<UserEntity> optionalEntity =  userDao.findByFirstName("abc");
-	    UserEntity checkFirstname= optionalEntity.get();
-	    assertThat(checkFirstname).isNotNull();
-	    
- 
-	}
-	
-	@Test
-	void testUpdateEmail() {
-		UserEntity user =  new UserEntity(100, "a","b","c","d", "d");
-           
-	    assertThat(user).isNotNull();
-	    
-	    user.setEmail("email");
-	    user.setPassword("password");
-	    userDao.save(user);
-	    
-	    Optional<UserEntity> optionalEntity =  userDao.findByEmailAndPassword("email","password" );
-	    UserEntity checkFirstname= optionalEntity.get();
-	    assertThat(checkFirstname).isNotNull();
-	    
- 
-	}
+
+
+    @InjectMocks
+    UserService userService= new UserService();
+
+    @Mock
+    UserDao userDao ;
+
+    
+
+    
+
+    @Test
+    void testUpdateLast() {
+
+        Optional<UserEntity> dummy = Optional.ofNullable(new UserEntity(100, "test@test.com","password","test", "user","test"));
+
+        
+
+        String lastNameTest="userss";
+        dummy.get().setLastName(lastNameTest);
+        userService.save(dummy.get());
+        when(userDao.findByLastName( lastNameTest)).thenReturn(dummy);
+
+        Optional<UserEntity> expected = dummy;
+        Optional<UserEntity> actual =null;
+
+
+        actual= userDao.findByLastName("userss");
+
+        assertEquals(expected, actual);
+    }
+
+    
+
+    @Test
+    void testUpdateFirst() {
+    Optional<UserEntity> dummy = Optional.ofNullable(new UserEntity(100, "test@test.com","password","test", "user","test"));
+
+        String firstNameTest="userss";
+        dummy.get().setFirstName(firstNameTest);
+        userService.save(dummy.get());
+        when(userDao.findByFirstName( firstNameTest)).thenReturn(dummy);
+        
+        Optional<UserEntity> expected = dummy;
+        Optional<UserEntity> actual =null;
+       
+        actual= userDao.findByFirstName("userss");
+
+        assertEquals(expected, actual);       
+    }
+
+    
+
+    @Test
+    void testUpdateEmail() {
+
+    Optional<UserEntity> dummy = Optional.ofNullable(new UserEntity(100, "test@test.com","password","test", "user","test"));
+        
+        String emailTest="user@user.com";
+   
+        dummy.get().setEmail(emailTest);
+        userService.save(dummy.get());
+        when(userDao.findByEmailAndPassword( emailTest,"password" )).thenReturn(dummy);
+
+        
+
+        Optional<UserEntity> expected = dummy;
+        Optional<UserEntity> actual =null;
+
+        actual= userDao.findByEmailAndPassword(emailTest,"password" );
+
+        assertEquals(expected, actual);     
+    }
+
+    
+
+    
+
+
+
+
+
 }
